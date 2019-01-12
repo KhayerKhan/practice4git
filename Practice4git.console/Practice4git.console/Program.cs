dfbe51bcd86a960fe3a7bcf9d9f94451b45723df
Khayer Khan <coderboy1010@gmail.com>
2019-01-12 09:36:59 -0800

    Adding initial project files

---
+++ Practice4git.console/Practice4git.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice4git.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
