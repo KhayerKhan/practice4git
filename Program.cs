59277af1579d0d70a6bbf64d434e3c6a628a16ab
Khayer Khan <coderboy1010@gmail.com>
2019-02-25 19:56:04 -0800

    Adding Program.cs file for list generics

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection.Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> StudentName = new List<string>();
+
+            StudentName.Add("Abid");
+            StudentName.Add("Rasel");
+            StudentName.Add("Khayer");
+            StudentName.Add("Jahid");
+            
+            
+            foreach (string name in StudentName)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Total count:{0}",StudentName.Count) ;
+            Console.WriteLine("\nContains(\"Abid\"): {0}",
+            StudentName.Contains("Abid"));
+
+            Console.WriteLine("\nInsert(1, \"Rasel\")");
+            StudentName.Insert(1, "Hasan");
+
+            Console.WriteLine("insert 1 :{0} ",StudentName[1]);
+            Console.WriteLine("\nRemove(\"Hasan\")\n");
+            StudentName.Remove("Hasan\n");
+
+            foreach (string name in StudentName)
+                {
+                    Console.WriteLine(name);
+                }
+            StudentName.Clear();
+
+            Console.WriteLine("\nClear all Data: {0}",StudentName.Count);
+                Console.ReadLine();
+        }
+    }
+}
